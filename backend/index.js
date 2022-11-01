542de17156c7047ccdb67c656956d269ad95cdee
mraihazhari <muhammad.raihan03@ui.ac.id>
2022-11-01 23:54:50 +0700

    Inisialisasi passport

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,26 @@
+const cookieSession = require("cookie-session");
 const express = require("express");
+const passport = require("passport");
 const app = express()
+const cors = require("cors");
 
 console.log("test");
+
+app.use(cookieSession({
+    name:"session",
+    keys:["yellowvote"],
+    maxAge: 24 * 60 * 60 * 100
+}
+))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cors({
+    origin:"http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+}))
 
 app.listen("5000", ()=>{
     console.log("Server is running!");
