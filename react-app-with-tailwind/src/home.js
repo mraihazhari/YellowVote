4c254de3eee6a44bc630d5acf43454e9edee189a
rolandmaulana <rolandmaulana8@gmail.com>
2022-11-06 20:23:20 +0700

    gua tinggal nonton emyu dulu ya

--- react-app-with-tailwind/src/home.js
+++ react-app-with-tailwind/src/home.js
@@ -1,5 +1,4 @@
-import { Fragment } from 'react'
-import { Popover, Transition, Disclosure } from '@headlessui/react'
+import { Disclosure } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import React from "react";
 import { Helmet } from "react-helmet";
--- react-app-with-tailwind/src/landing.js
+++ react-app-with-tailwind/src/landing.js
@@ -1,44 +1,121 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import React from "react";
+import { Disclosure } from '@headlessui/react'
 import { Helmet } from "react-helmet";
 
+const navigation = [
+  { name: 'YellowVote', href: './home', current: true },
+]
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 function Landing() {
-  return (
+  return (   
     
-    <div className="relative overflow-hidden bg-yellow-300">
+      <div className="min-h-full">
         <Helmet>
-        <style>{"body { background-color: rgb(20 30 199); }"}</style>
+        <style>{"body { background-color: rgb(253 224 71); }"}</style>
         </Helmet>
-      <div className="mx-auto max-w-7xl">
-        <div className="relative z-10 bg-yellow-300 pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
-          <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <Disclosure as="nav" className="bg-blue-800">
+          {({ open }) => (
+            <>
+              <div className="max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-14 items-center justify-between">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0">
+                      
+                    </div>
+                    <div className="hidden md:block">
+                      <div className="ml-0 flex items-baseline space-x-2">
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? 'bg-yellow-300 text-blue-800'
+                                : 'text-yellow-300 hover:bg-yellow-300 hover:text-blue-800',
+                              'px-3 py-2 rounded-md text-sm font-medium'
+                            )}
+                            aria-current={item.current ? 'page' : undefined}
+                          >
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="-mr-2 flex md:hidden">
+                    {/* Mobile menu button */}
+                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                      )}
+                    </Disclosure.Button>
+                  </div>
+                </div>
+              </div>
+
+              <Disclosure.Panel className="md:hidden">
+                <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+                  {navigation.map((item) => (
+                    <Disclosure.Button
+                      key={item.name}
+                      as="a"
+                      href={item.href}
+                      className={classNames(
+                        item.current ? 'bg-yellow-300 text-blue-800' : 'text-yellow-300 hover:bg-yellow-300 hover:text-blue-800',
+                        'block px-3 py-2 rounded-md text-base font-medium'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </Disclosure.Button>
+                  ))}
+                </div>
+                
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+
+        <main className="py-36 h-screen mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 ">
             <div className="sm:text-center lg:text-center">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-8xl">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
                 <span className="block text-indigo-800 xl:inline">Welcome To YellowVote!</span>
               </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-14 md:text-3xl lg:mx-0">
-              YellowVote is an online voting platform made exclusively for the Universitas Indonesia community
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
+                <p className="mt-3 text-base text-indigo-800 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
+                YellowVote is an online voting platform made exclusively for the Universitas
+                Indonesia community.
               </p>
-              <div className="mt-5 sm:mt-32 sm:flex sm:justify-center lg:justify-start">
+              </div>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
                 <div className="rounded-md shadow">
                   <a
-                    href="./home"
-                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
+                    href="#"
+                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-blue-700 px-8 py-3 text-base font-medium text-yellow-300 hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
                   >
-                    Login
+                    Log in with Office
+                  </a>
+                </div>
+                <div className="mt-3 sm:mt-0 sm:ml-3">
+                  <a
+                    href="#"
+                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-100 px-8 py-3 text-base font-medium text-blue-700 hover:bg-indigo-200 md:py-4 md:px-10 md:text-lg"
+                  >
+                    Log in with Google
                   </a>
                 </div>
               </div>
             </div>
           </main>
-        </div>
       </div>
-    </div>
   )
 }
 
