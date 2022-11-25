d7163694c33e5111ea1e39166413e87be56df4b4
rolandmaulana <rolandmaulana8@gmail.com>
2022-11-25 13:08:42 +0700

    addchoice

--- react-app-with-tailwind/src/CreatePoll2.js
+++ react-app-with-tailwind/src/CreatePoll2.js
@@ -88,6 +88,118 @@ function CreatePoll2() {
             </>
           )}
         </Disclosure>
+        <header className="bg-yellow-300">
+          <div className="max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-5xl font-bold tracking-tight text-indigo-800">Add A Choice</h1>
+          </div>
+        </header>
+        <main className="bg-yellow-300">
+        <div className="mt-5 md:col-span-2 md:mt-0">
+            <form action="#" method="POST">
+              <div className="shadow sm:overflow-hidden sm:rounded-md">
+                <div className="space-y-6 bg-yellow-300 px-4 py-5 sm:p-6">
+
+                  <div className="grid grid-cols-9 gap-6">
+                    <div className="col-span-3 sm:col-span-2">
+                      <label htmlFor="number" className="block text-sm font-medium text-indigo-800">
+                        Number
+                      </label>
+                      <div className="mt-1 flex rounded-md shadow-sm">
+                        <input
+                          // onChange={(e)=>handle(e)}
+                          // value={data.title}
+                          type="number"
+                          name="number"
+                          id="number"
+                          className="block w-full flex-1 rounded-md border-gray-300 focus:border-indigo-800 focus:ring-indigo-800 sm:text-sm"
+                          placeholder="Enter number here"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-3 gap-6">
+                    <div className="col-span-3 sm:col-span-2">
+                      <label htmlFor="name" className="block text-sm font-medium text-indigo-800">
+                        Name
+                      </label>
+                      <div className="mt-1 flex rounded-md shadow-sm">
+                        <input
+                          // onChange={(e)=>handle(e)}
+                          // value={data.title}
+                          type="text"
+                          name="name"
+                          id="name"
+                          className="block w-full flex-1 rounded-md border-gray-300 focus:border-indigo-800 focus:ring-indigo-800 sm:text-sm"
+                          placeholder="Enter name here"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <label htmlFor="description" className="block text-sm font-medium text-indigo-800">
+                      Description
+                    </label>
+                    <div className="mt-1">
+                      <textarea
+                        // onChange={(e)=>handle(e)}
+                        // value={data.description}
+                        id="description"
+                        name="description"
+                        rows={3}
+                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-800 focus:ring-indigo-800 sm:text-sm"
+                        placeholder="Enter description here"
+                        defaultValue={''}
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label className="py-5 block text-sm font-medium text-indigo-800">Photo</label>
+                    <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-blue-700 py-12 px-6 pt-5 pb-6">
+                      <div className="space-y-1 text-center">
+                        <svg
+                          className="mx-auto h-12 w-12 text-indigo-800"
+                          stroke="currentColor"
+                          fill="none"
+                          viewBox="0 0 48 48"
+                          aria-hidden="true"
+                        >
+                          <path
+                            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                            strokeWidth={2}
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                        <div className="flex text-sm text-gray-600">
+                          <label
+                            htmlFor="file-upload"
+                            className="relative cursor-pointer rounded-md bg-yellow-300 font-medium text-blue-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+                          >
+                            <span>Upload a file</span>
+                            <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                          </label>
+                          <p className="pl-1">or drag and drop</p>
+                        </div>
+                        <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                  <div className="bg-yellow-300 px-4 py-8 text-right sm:px-6">
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-700 py-2 px-4 text-sm font-medium text-yellow-300 shadow-sm hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:ring-offset-2"
+                    >
+                      Add Choice
+                    </button>
+                  </div>
+                </div>
+            </form>
+          </div>
+        </main>
         </div> 
     </>
     )
